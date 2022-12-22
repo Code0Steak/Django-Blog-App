95f00a2b44baecbeded89ed9369cfae025dab5ec
Ameya Keskar <mycodestreak@gmail.com>
2022-12-22 21:19:48 +0530

    Declared a Model 'Post'. Then created a few Posts for a User. The users were added, earlier, to the auth_user table using Django Admin dashboard, avlb. on localhost:8000/admin

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User #From Django's default user and auth system
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now) #on post creation, the default date and time in the timzeone will be set for the post.
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #User will have a one-many relation with Post
+    
+    def __str__(self):
+        return self.title 
