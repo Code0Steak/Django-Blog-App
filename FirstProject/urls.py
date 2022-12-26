1c4afeba58d5d20545525272087003db3a578031
Ameya Keskar <mycodestreak@gmail.com>
2022-12-26 18:35:36 +0530

    Profile model for a user is auto-created after user registeration. Updated the default location for profile image storage. Profile image is displayed on the Profile page

--- FirstProject/urls.py
+++ FirstProject/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 from UserApp import views as userViews
 from django.contrib.auth import views as auth_views
 
+#following imports are for profile pic media
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('BlogApp.urls')),
@@ -26,3 +30,6 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), 
     path('profile/',userViews.profile, name='profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,)
