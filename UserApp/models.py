e4793dd05906d10d81a65fa1379d11af75ce9ef7
Ameya Keskar <mycodestreak@gmail.com>
2023-01-04 10:52:54 +0530

    save function for Profile model overriden to implement image resize functionality

--- UserApp/models.py
+++ UserApp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #if the User is deleted the profile for the user will be gone. But not viceversa.
@@ -7,3 +9,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile for {self.user.username}'
+    
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            outputSize = (300,300)
+            img.thumbnail(outputSize)
+            img.save(self.image.path)
